2eb1778a76e0fca4dabd3355c84731849b12a393
Fernanda <fe.perezl@duocuc.cl>
2024-07-23 18:26:26 -0400

    ADD: variables and update script source in index.html

--- 01-fundamentos/app.js
+++ 01-fundamentos/app.js
@@ -1,2 +1,13 @@
+/* console.log("Hot Wheels");
+console.log("Hola mundo, esta será mi web de hotwheels"); */
 
-console.log("Hot Wheels")
+// variables
+
+let a = 10;
+var b = 10;
+
+const c = 10; // este valor no puede cambiar
+
+// c = 20; no se puede hacer esto, dará error
+a = 14;
+b = 15;
