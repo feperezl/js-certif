76e1010123017cc38b53a1e6266fd7e61570f139
Fernanda <fe.perezl@duocuc.cl>
2024-07-23 18:39:27 -0400

    ADD: variable declarations and console logs in app.js

--- 01-fundamentos/app.js
+++ 01-fundamentos/app.js
@@ -1,13 +1,26 @@
 /* console.log("Hot Wheels");
-console.log("Hola mundo, esta será mi web de hotwheels"); */
+console.log("Hola mundo, esta será mi web de hotwheels");
 
 // variables
 
-let a = 10;
+ let a = 10;
 var b = 10;
 
 const c = 10; // este valor no puede cambiar
 
 // c = 20; no se puede hacer esto, dará error
 a = 14;
-b = 15;
+b = 15; */
+
+let a = 10;
+    b = 20;
+    c = 30;
+    d = 40;
+    x = a + b;
+
+/* console.log('a',a);
+console.log('b',b);
+console.log('c',c); */
+
+console.log({ a, b, c })
+console.log('%c Mis variables', 'color:blue; font-weight: thin');
